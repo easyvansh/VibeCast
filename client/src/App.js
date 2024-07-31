8a611d78228c1caf6ff914e89dc1b2b7dec1d674
Vansh Singh <vansh2@ualberta.ca>
2024-07-31 19:12:57 +0530

    Added Theme Toggle

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,29 @@
+import {useState} from "react";
+import styled from "styled-components";
+import {ThemeProvider} from "styled-components";
+import "./index.css";
+import {lightTheme, darkTheme} from "./utils/Theme"
+import Sidebar from "./components/Sidebar";
 
+const Container = styled.div`
+  display:flex;
+  background:${({theme}) =>theme.bg};
+  width:100%;
+  height:100vh;
+  overflow-x:hidden;
+  overflow-y:hidden;
+`;
 
 function App() {
+  // hooks
+  const [darkMode, setDarkMode] = useState(true);
   return (
-    <div className="App">
-      VibeCast
-    </div>
+    <ThemeProvider theme ={darkMode ? darkTheme : lightTheme}>
+      <Container>
+        <Sidebar/>
+        VibeCast
+      </Container>
+    </ThemeProvider>
   );
 }
 

8ccec77913d13a54943d630dfa617580a795fc8c
Vansh Singh <vansh2@ualberta.ca>
2024-08-04 14:25:06 +0530

    Updated Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
-import {useState} from "react";
+import { useState } from "react";
 import styled from "styled-components";
-import {ThemeProvider} from "styled-components";
+import { ThemeProvider } from "styled-components";
 import "./index.css";
-import {lightTheme, darkTheme} from "./utils/Theme"
+import { lightTheme, darkTheme } from "./utils/Theme";
 import Sidebar from "./components/Sidebar";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
@@ -13,20 +13,19 @@ import Profile from "./pages/Profile";
 import PodcastDetails from "./pages/PodcastDetails";
 import DisplayPodcast from "./pages/DisplayPodcast";
 
-
 const Container = styled.div`
-  display:flex;
-  background:${({theme}) =>theme.bgLight};
-  width:100%;
-  height:100vh;
-  overflow-x:hidden;
-  overflow-y:hidden;
+  display: flex;
+  background: ${({ theme }) => theme.bgLight};
+  width: 100%;
+  height: 100vh;
+  overflow-x: hidden;
+  overflow-y: hidden;
 `;
 
 const Frame = styled.div`
-  display:flex;
-  flex-direction:column;
-  flex:3;
+  display: flex;
+  flex-direction: column;
+  flex: 3;
 `;
 
 function App() {
@@ -34,32 +33,34 @@ function App() {
   const [darkMode, setDarkMode] = useState(true);
   const [menuOpen, setMenuOpen] = useState(true);
   return (
-    <ThemeProvider theme ={darkMode ? darkTheme : lightTheme}>
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
       <BrowserRouter>
-      <Container>
-        {menuOpen && (
-        <Sidebar 
-        menuOpen={menuOpen}
-        setMenuOpen = {setMenuOpen}
-        setDarkMode = {setDarkMode}
-        darkMode = {darkMode}/>
-        )}
-        <Frame>
-          <Navbar
-          menuOpen={menuOpen}
-          setMenuOpen = {setMenuOpen}
-          />
-        VibeCast
-        <Routes>
-        <Route path='/' exact element={<Dashboard />} />
-        <Route path='/search' exact element={<Search />} />
-        <Route path='/favourites' exact element={<Favourites />} />
-        <Route path='/profile' exact element={<Profile />} />
-        <Route path='/podcast/:id' exact element={<PodcastDetails />} />
-        <Route path='/showpodcasts/:type' exact element={<DisplayPodcast/>} />
-        </Routes>
-        </Frame>
-      </Container>
+        <Container>
+          {menuOpen && (
+            <Sidebar
+              menuOpen={menuOpen}
+              setMenuOpen={setMenuOpen}
+              setDarkMode={setDarkMode}
+              darkMode={darkMode}
+            />
+          )}
+          <Frame>
+            <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+           
+            <Routes>
+              <Route path="/" exact element={<Dashboard />} />
+              <Route path="/search" exact element={<Search />} />
+              <Route path="/favourites" exact element={<Favourites />} />
+              <Route path="/profile" exact element={<Profile />} />
+              <Route path="/podcast/:id" exact element={<PodcastDetails />} />
+              <Route
+                path="/showpodcasts/:type"
+                exact
+                element={<DisplayPodcast />}
+              />
+            </Routes>
+          </Frame>
+        </Container>
       </BrowserRouter>
     </ThemeProvider>
   );

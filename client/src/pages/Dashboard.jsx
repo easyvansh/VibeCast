482039758c03ec0c909cf9d97809e087638c42d4
Vansh Singh <vansh2@ualberta.ca>
2024-08-04 14:25:46 +0530

    Updated Dashboard - Added CSS and Home Functionalities

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,9 +1,136 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import PodcastCard from "../components/PodcastCard";
 
+const DashboardMain = styled.div`
+  padding: 20px 30px;
+  padding-bottom: 200px;
+  height: 100%;
+  overflow-y: scroll;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  @media (max-width: 768px) {
+    padding: 6px 10px;
+  }
+`;
+const FilterContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  ${({ box, theme }) =>
+    box &&
+    `
+background-color: ${theme.bg};
+  border-radius: 10px;
+  padding: 20px 30px;
+`}
+  background-color: ${({ theme }) => theme.bg};
+  border-radius: 10px;
+  padding: 20px 30px;
+`;
+const Topic = styled.div`
+  color: ${({ theme }) => theme.text_primary};
+  font-size: 24px;
+  font-weight: 540;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  @maedia (max-width: 768px) {
+    font-size: 18px;
+  }
+`;
+const Span = styled.span`
+  color: ${({ theme }) => theme.text_secondary};
+  font-size: 16px;
+  font-weight: 400;
+  cursor: pointer;
+  @media (max-width: 768px) {
+    font-size: 14px;
+  }
+  color: ${({ theme }) => theme.primary};
+  &:hover {
+    transition: 0.2s ease-in-out;
+  }
+`;
+const Podcasts = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, 240px);
+  gap: 16px;
+  padding: 18px 6px;
+  //center the items if only 2 item present
+  @media (max-width: 1200px) {
+    justify-content: center;
+  }
+`;
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
-  )
-}
+    <DashboardMain>
+      <FilterContainer>
+        <Topic>
+          Most Popular
+          <Link
+            to={`/showpodcasts/mostpopular`}
+            style={{ textDecoration: "none" }}
+          >
+            <Span>Show All</Span>
+          </Link>
+        </Topic>
+        <Podcasts>
+          <PodcastCard/>
+          <PodcastCard/>
+          <PodcastCard/>
+          <PodcastCard/>
+        </Podcasts>
+      </FilterContainer>
 
-export default Dashboard
+      <FilterContainer>
+        <Topic>
+          Comedy
+          <Link to={`/showpodcasts/comedy`} style={{ textDecoration: "none" }}>
+            <Span>Show All</Span>
+          </Link>
+        </Topic>
+        <Podcasts>
+        <PodcastCard/>
+        <PodcastCard/>
+        <PodcastCard/>
+        <PodcastCard/>
+        </Podcasts>
+      </FilterContainer>
+      <FilterContainer>
+        <Topic>
+          News
+          <Link to={`/showpodcasts/news`} style={{ textDecoration: "none" }}>
+            <Span>Show All</Span>
+          </Link>
+        </Topic>
+        <Podcasts>
+        <PodcastCard/>
+        <PodcastCard/>
+        <PodcastCard/>
+        <PodcastCard/>
+        </Podcasts>
+      </FilterContainer>
+      <FilterContainer>
+        <Topic>
+          Most Popular
+          <Link
+            to={`/showpodcasts/mostpopular`}
+            style={{ textDecoration: "none" }}
+          >
+            <Span>Show All</Span>
+          </Link>
+        </Topic>
+        <Podcasts>
+        <PodcastCard/>
+        <PodcastCard/>
+        <PodcastCard/>
+        <PodcastCard/>
+        </Podcasts>
+      </FilterContainer>
+    </DashboardMain>
+  );
+};
+
+export default Dashboard;
